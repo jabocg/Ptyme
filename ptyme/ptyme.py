78faf6070e3b4d079b2be5f430e7817022582e11
jabocg <jabocg@gmail.com>
2016-02-17 00:30:12 -0700

    beginnings of code

--- ptyme/ptyme.py
+++ ptyme/ptyme.py
@@ -1,7 +1,31 @@
-# empty files are the best
+#!/bin/env python3
+
+from sys import argv
 
 def main():
+    parseArgs()
     print("Nope.")
+    print(argv)
+
+def parseArgs():
+    if len(argv) > 1:
+        time = argv[1].split('h')
+        print(time)
+        if 'm' not in time[0] and 'n' not in time[0]:
+            hours = time[0]
+        else:
+            house = 0
+        time = time[0].split('m')
+        print(time)
+        if 's' not in time[0]:
+            minutes = time[0]
+        else:
+            minutes = 0
+        time = time[0].split('s')
+        if time:
+            seconds = time[0]
+    else:
+        print("commands go here")
 
 if __name__ == "__main__":
     main()
