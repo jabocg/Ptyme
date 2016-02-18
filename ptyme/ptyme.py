159e1e210480c0037b3a550e70b77dbfce34bbca
jabocg <jabocg@gmail.com>
2016-02-18 11:42:08 -0700

    Update current parse, add alt parse frame

--- ptyme/ptyme.py
+++ ptyme/ptyme.py
@@ -7,23 +7,35 @@ def main():
     print("Nope.")
     print(argv)
 
+# go through via characters
+def parseArgsChar():
+    pass()
+
+# while this works, it only works when _h_m_s format
+# might want to not do that
 def parseArgs():
     if len(argv) > 1:
         time = argv[1].split('h')
-        print(time)
         if 'm' not in time[0] and 'n' not in time[0]:
             hours = time[0]
         else:
-            house = 0
-        time = time[0].split('m')
+            hours = 0
         print(time)
+        print(hours)
+        time = time[1].split('m')
         if 's' not in time[0]:
             minutes = time[0]
         else:
             minutes = 0
-        time = time[0].split('s')
+        print(time)
+        print(minutes)
+        time = time[1].split('s')
         if time:
             seconds = time[0]
+        else:
+            seconds = 0
+        print(time)
+        print(seconds)
     else:
         print("commands go here")
 
