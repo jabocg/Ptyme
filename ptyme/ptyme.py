b45ab1d82ec3a2ac0380fa4fef713fe4e62357e3
Jacob Gersztyn <jabocg@gmail.com>
2016-07-18 14:25:06 -0700

    Add easygui, give nap capability, make python2
    This is just a quick little throw-together so I can sleep/use a timer
    form the command line. This is probably going to be overridden
    eventually. For now, it is a floating boat made of scrap. But it floats.
    I will probably use a different GUI framework as well, so I can keep
    python3 abilities.

--- ptyme/ptyme.py
+++ ptyme/ptyme.py
@@ -1,43 +1,60 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+# easygui doesn't work with python3, using python 2 for now
 # python 3
 
 import sys
-import logging
+# import logging
+import time
+import easygui as gui
 
-logging.baseConfig(format='%(asctime)s %(message)s',
-                   dateftm='%Y-%m-%dT%H:%M:%S')
+# logging.baseConfig(format='%(asctime)s %(message)s',
+#                    dateftm='%Y-%m-%dT%H:%M:%S')
 
 def main():
-    print(sys.argv)
+    # print(sys.argv)
     if len(sys.argv) == 1:
         print("give a string for time, _h_m_s for hours, mins, secs, optional"
               "string for title")
         sys.exit()
-    parseArgsChar(sys.argv[1])
+    timetosleep = parseArgsChar(sys.argv[1])
+    nap(timetosleep)
 
+def nap(duration):
+    # print("napping for " + str(duration) + " seconds")
+    wakeup = False
+    while not wakeup:
+        # print(duration)
+        time.sleep(1)
+        duration -= 1
+        wakeup = duration <= 0
+    # print("you have napped")
+    gui.msgbox("wake up")
 
 # go through via characters
 def parseArgsChar(timeStr):
     hours = minutes = seconds = 0
     head = 0
     for ind, char in enumerate(timeStr):
-        print(ind, char, head)
+        # print(ind, char, head)
         if char == 'h':
-            print("found h")
-            hours = timeStr[head:ind]
+            # print("found h")
+            hours = int(timeStr[head:ind])
             head = ind + 1
-        if char == 'm':
-            print("found m")
-            minutes = timeStr[head:ind]
+        elif char == 'm':
+            # print("found m")
+            minutes = int(timeStr[head:ind])
             head = ind + 1
-        if char == 's':
-            print("found s")
-            seconds = timeStr[head:ind]
+        elif char == 's':
+            # print("found s")
+            seconds = int(timeStr[head:ind])
             head = ind + 1
-    print(timeStr)
-    print('hours:', hours)
-    print('mins: ', minutes)
-    print('sec: ', seconds)
+        elif char.isalpha():
+            head = ind + 1
+    # print(timeStr)
+    # print('hours:' + str(hours))
+    # print('mins: ' + str(minutes))
+    # print('sec: ' + str(seconds))
+    return hours * 3600 + minutes * 60 + seconds
 
 
 if __name__ == "__main__":
